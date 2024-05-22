dd04957fee685e85e6f62655aeac1feebcf11011
rickherrera3001 <rihr.3001@gmail.com>
2024-05-22 14:10:15 -0600

    Create Textfield email

--- StreamFlow/Modules/HomeViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HomeViewModel.swift
-//  StreamFlow
-//
-//  Created by Ricardo Developer on 21/05/24.
-//
-
-import Foundation
--- StreamFlow/Modules/LoginView.swift
+++ StreamFlow/Modules/LoginView.swift
@@ -1,18 +1,50 @@
-//
-//  LoginView.swift
-//  StreamFlow
-//
-//  Created by Ricardo Developer on 21/05/24.
-//
-
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var contraseña: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color(red: 18/255, green: 31/255, blue: 61/255).ignoresSafeArea()
+            
+            VStack {
+                Image("App Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 110)
+                    .padding(.bottom, 60.0)
+                    .padding(.top, 50) 
+                
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .foregroundColor(Color(red: 63/255, green: 202/255, blue: 160/255, opacity: 1.0))
+                    
+                    ZStack(alignment: .leading) {
+                        if email.isEmpty {
+                            Text("ejemplo@gmail.com")
+                                .font(.caption)
+                                .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
+                        }
+                        
+                        TextField("", text: $email)
+                            .padding()
+                            .background(Color.gray)
+                            .cornerRadius(5.0)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+        }
     }
 }
 
 #Preview {
     LoginView()
 }
+
