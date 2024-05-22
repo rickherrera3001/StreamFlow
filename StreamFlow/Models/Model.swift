a880fc6754b3b43336389d4c1a404c5081392ddb
rickherrera3001 <rihr.3001@gmail.com>
2024-05-21 22:21:21 -0600

    first commit

---
+++ StreamFlow/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  StreamFlow
+//
+//  Created by Ricardo Developer on 21/05/24.
+//
+
+import Foundation
---
+++ StreamFlow/Modules/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  StreamFlow
+//
+//  Created by Ricardo Developer on 21/05/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
