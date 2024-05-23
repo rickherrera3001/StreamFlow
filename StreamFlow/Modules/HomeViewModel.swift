a06cfcdc28da7bd75753fafb5608de11bc6021c9
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-05-22 23:45:50 -0600

    HomeViewModel

--- StreamFlow/Modules/HomeViewModel.swift
+++ StreamFlow/Modules/HomeViewModel.swift
@@ -5,7 +5,33 @@
 //  Created by Ricardo Developer on 21/05/24.
 //
 
+import Foundation
+import Combine
 
+class MoviesViewModel: ObservableObject {
+    @Published var movies: [Movies] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+
+    private var cancellables = Set<AnyCancellable>()
+    
+    func fetchMovies(page: Int) {
+        isLoading = true
+        errorMessage = nil
+        
+        APIClient.shared.fetchMovies(page: page) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result {
+                case .success(let movieModel):
+                    self?.movies = movieModel.results ?? []
+                case .failure(let error):
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
 
 
 
